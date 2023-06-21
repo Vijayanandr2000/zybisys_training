85f2704bc26b4bdd18f1ed31f29c7375e71fb1c6
vijayanand <vijayanandr2000@gmail.com>
2023-06-21 11:38:59 +0530

    code added

--- JS/week2/demo/app.js
+++ JS/week2/demo/app.js
@@ -1,5 +1,5 @@
-import { addExpenses , expenses} from "./modules/expensesModule.js"
-import { displayList} from "./modules/uiModule.js"
+import { addExpenses, expenses } from "./modules/expensesModule.js"
+import { displayList } from "./modules/uiModule.js"
 
 let expensesFormContainer = document.getElementById("expenses-form");
 let expensesListContainer = document.getElementById("expenses-list");
@@ -7,6 +7,7 @@ let expensesListContainer = document.getElementById("expenses-list");
 fetch('./components/expenses-form.html').then((response) => response.text())
     .then((html) => {
         expensesFormContainer.innerHTML = html
+        fetchForms()
     })
 
 fetch('./components/expenses-list.html').then((response) => response.text())
@@ -14,26 +15,27 @@ fetch('./components/expenses-list.html').then((response) => response.text())
         expensesListContainer.innerHTML = html
     })
 
-let form = document.getElementById("expenses-form")
-form.addEventListener("submit", (event) => {
-    try {
-        event.preventDefault();
+function fetchForms() {
+    // Move the fetchForm() function outside of fetchForms()
+    let form  = document.getElementById('click');
+    form.addEventListener('click', fetchForm)
+    function fetchForm(e) {
+        try {
+            e.preventDefault()
+            let expenseNameInput = document.getElementById("expenseName");
+            let expensePriceInput = document.getElementById("expensePrice");
 
-        let expenseNameInput = document.getElementById("expenseName")
-        let expensePriceInput = document.getElementById("expensePrice")
+            let expenseName = expenseNameInput.value;
+            let expensePrice = expensePriceInput.value;
 
+            if (expenseName && expensePrice) {
+                addExpenses(expenseName, expensePrice);
+                console.log("expenses", expenses);
 
-        let expenseName = expenseNameInput.value
-        let expensePrice = expensePriceInput.value
-
-        if (expenseName && expensePrice) {
-            addExpenses(expenseName, expensePrice)
-            console.log("expenses", expenses)
-
-            displayList()
+                displayList();
+            }
+        } catch (error) {
+            alert(`App Error: ${error.message}`);
         }
-
-    } catch (error) {
-        alert(`App Error: ${error.message}`)
     }
-})
+}
---
+++ JS/week3/AdvFun/advFn.js
@@ -0,0 +1,81 @@
+// Project: "Event Tracker"
+
+// Description:
+// Create an event tracker application that allows users to manage their events. The application should utilize advanced JavaScript functions and closures to provide functionality such as adding events, displaying events, and filtering events based on different criteria.
+
+// Requirements:
+// 1. Implement a function to add events. Each event should have a title, date, and location.
+// 2. Create a function to display all events, showing the title, date, and location of each event.
+// 3. Implement a function to filter events based on a specified date range.
+// 4. Create a closure that keeps track of the total number of events added.
+// 5. Implement a function to display the total number of events.
+// 6. Use higher-order functions such as `map`, `filter`, or `reduce` for manipulating and displaying events.
+// 7. Utilize closures to ensure data privacy and encapsulation of event-related information.
+
+// Example Usage:
+// ```javascript
+// Add events
+// addEvent("Meeting 1", "2023-06-21", "Conference Room A");
+// addEvent("Meeting 2", "2023-06-22", "Conference Room B");
+// addEvent("Workshop", "2023-06-23", "Training Room");
+
+// Display all events
+// displayEvents();
+// Output:
+// Event: Meeting 1
+// Date: 2023-06-21
+// Location: Conference Room A
+// //
+// Event: Meeting 2
+// Date: 2023-06-22
+// Location: Conference Room B
+// //
+// Event: Workshop
+// Date: 2023-06-23
+// Location: Training Room
+
+// Filter events by date range
+// const filteredEvents = filterEvents("2023-06-21", "2023-06-23");
+// displayEvents(filteredEvents);
+// Output:
+// Event: Meeting 1
+// Date: 2023-06-21
+// Location: Conference Room A
+// //
+// Event: Meeting 2
+// Date: 2023-06-22
+// Location: Conference Room B
+
+// Display the total number of events
+// displayTotalEvents();
+// Output: Total Events: 3
+// ```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
