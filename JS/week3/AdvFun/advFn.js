261d672cc6a9228329f746ccd745018a8ce53f28
vijayanand <vijayanandr2000@gmail.com>
2023-06-22 10:13:35 +0530

    code added

--- JS/week3/AdvFun/advFn.js
+++ JS/week3/AdvFun/advFn.js
@@ -52,7 +52,70 @@
 // ```
 
 
+// // Event Tracker Application
 
+// class EventTracker {
+//     constructor() {
+//       this.events = [];
+//       this.totalEvents = 0;
+//     }
+  
+//     // Function to add events
+//     addEvent(title, date, location) {
+//       const event = {
+//         title,
+//         date,
+//         location,
+//       };
+//       this.events.push(event);
+//       this.totalEvents++;
+//     }
+  
+//     // Function to display all events or filtered events
+//     displayEvents(filteredEvents = null) {
+//       const eventsToDisplay = filteredEvents || this.events;
+  
+//       eventsToDisplay.forEach((event) => {
+//         console.log(`Event: ${event.title}`);
+//         console.log(`Date: ${event.date}`);
+//         console.log(`Location: ${event.location}`);
+//         console.log();
+//       });
+//     }
+  
+//     // Function to filter events based on date range
+//     filterEvents(startDate, endDate) {
+//       return this.events.filter((event) => {
+//         return event.date >= startDate && event.date <= endDate;
+//       });
+//     }
+  
+//     // Function to display the total number of events
+//     displayTotalEvents() {
+//       console.log(`Total Events: ${this.totalEvents}`);
+//     }
+//   }
+  
+//   // Create an instance of EventTracker
+//   const eventTracker = new EventTracker();
+  
+//   // Add events
+//   eventTracker.addEvent("Meeting 1", "2023-06-21", "Conference Room A");
+//   eventTracker.addEvent("Meeting 2", "2023-06-22", "Conference Room B");
+//   eventTracker.addEvent("Workshop", "2023-06-23", "Training Room");
+  
+//   // Display all events
+//   console.log("All Events:");
+//   eventTracker.displayEvents();
+  
+//   // Filter events by date range
+//   console.log("Filtered Events:");
+//   const filteredEvents = eventTracker.filterEvents("2023-06-21", "2023-06-23");
+//   eventTracker.displayEvents(filteredEvents);
+  
+//   // Display the total number of events
+//   eventTracker.displayTotalEvents();
+  
 
 
 
