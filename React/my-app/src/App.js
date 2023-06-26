936a5e74cd38497d207a178bb70675e4d6f27af6
vijayanand <vijayanandr2000@gmail.com>
2023-06-26 11:56:33 +0530

    React code added

---
+++ React/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDo from "./Components/ToDo"
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <ToDo name="VIjay"/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ React/my-app/src/Components/ToDo.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+
+
+const ToDo = ({name}) => {
+    // console.log("name", name)
+    let [todo, setTodo] = useState("")
+    
+    let [todoList, setTodoList] = useState([])
+
+    const handleChange = (e) => {
+        console.log("event", e)
+        setTodo(e.target.value)
+    }
+
+    const add = () => {
+        setTodoList([...todoList,todo])
+    }
+
+    useEffect(() =>{
+        console.log("name", name, todo,todoList) 
+    })
+
+    console.log("state", todo, todoList)
+    return (
+        <div>
+            <input type="text" required  onChange={handleChange}/>
+            <button onClick={() => {
+                add()
+            }}>Add</button>
+            
+
+           {/* <ul> */}
+           {todoList.map((todo, index) =>(
+                <li>{todo}</li>
+            ))}
+           {/* </ul> */}
+        </div>
+    )
+}
+
+export default ToDo;
